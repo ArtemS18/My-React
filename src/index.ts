840f92658861b15ddf1ada409cb93554f708154f
Artemiy <artemsikor18@gmail.com>
2026-04-11 15:06:22 +0300

    patch(1.0.4)

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export {createApp} from "./my-react";
 export  {type JSX} from "./types/jsx";
 export { useState, useEffect } from "./hooks";
 export * from "./jsx-runtime";
-export * from "./router-dom";
