ed339055704cace56d0062ebcd52a4fbc6d5c686
Artemiy <artemsikor18@gmail.com>
2026-04-11 15:03:21 +0300

    patch(1.0.2): delete router-dom

--- src/router-dom/hooks.ts
+++
@@ -1,8 +0,0 @@
-export function useNavigate() {
-  const navigate = (path: string) => {
-    console.log(path)
-    window.history.pushState({}, '', path);
-    window.dispatchEvent(new PopStateEvent('popstate'));
-  };
-  return navigate;
-}
--- src/router-dom/index.ts
+++
@@ -1,2 +0,0 @@
-export { Router, Switch } from "./Router";
-export { useNavigate } from "./hooks";
