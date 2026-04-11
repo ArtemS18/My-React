5a6e635d01a6b54286ba6be73a119918b80fbb57
Artemiy <artemsikor18@gmail.com>
2026-04-11 15:12:50 +0300

    patch(1.0.5): add jsx type imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {createApp} from "./my-react";
-export  {type JSX} from "./types/jsx";
+export  type {JSX, JSXComponent, JSXElement, JSXElementType} from "./types/jsx";
 export { useState, useEffect } from "./hooks";
 export * from "./jsx-runtime";
